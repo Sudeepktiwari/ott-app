79153896edf3f17d2f800c5dd8f771866f578cf2
Sudeepktiwari <sudeep@appointy.com>
2024-12-05 11:35:20 +0530

    searchbar

---
+++ ott-app-front/src/components/page-elements/search-bar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Type in the name to search"
+        className="border-solid border-white"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
